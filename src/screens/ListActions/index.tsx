1dd594c793dfb168a85c2809cfa5657ddff680ed
Rafinhaa <rafinhaa@me.com>
2023-02-23 22:53:35 -0300

    feat: Cria screens base Home e ListActions

---
+++ src/screens/ListActions/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export const ListActions: React.FC = () => {
+  return (
+    <View>
+      <Text>ListActions</Text>
+    </View>
+  );
+};
